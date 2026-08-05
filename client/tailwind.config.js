488d03d1005d669dbc5440e56b3262fe2bc82c50
Mahi Alam Azad <mahialamazads@gmail.com>
2026-08-06 02:19:04 +0600

    deleted tailwind sidebar hide

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,17 @@
-const config = {
-    content: [
-        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-        extend: {
-            colors: {
-                background: 'var(--background)',
-                foreground: 'var(--foreground)',
-            },
-        },
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
     },
-    plugins: [require("tailwind-scrollbar-hide"),],
+  },
+
+  plugins: [],
 };
-export default config;
